1466ef0f24522114ce2a3b4087534c270601134f
Yuliia <you@example.com>
2024-02-08 14:32:05 +0100

    Add component Title

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,14 @@
+import './Title.css'
+
+function Title() {
+    return (
+        <>
+            <div className="textHolder">
+                <h2 class="tipeSchool">Escuela de baile</h2>
+                <h1 class="nameSchool">Lola Flores</h1>
+            </div>
+
+        </>
+    )
+}
+export default Title;
