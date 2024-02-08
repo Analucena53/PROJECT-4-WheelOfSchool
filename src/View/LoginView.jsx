d951e1ba3acdf90f16f2f0d712de4c6995b25807
lidia pérez <ufonicap@gmail.com>
2024-02-08 15:35:29 +0100

    LoginView Title import

--- src/View/LoginView.jsx
+++ src/View/LoginView.jsx
@@ -1,11 +1,13 @@
 import Login from "../components/Login/Login";
-
+import Title from "../components/Title/Title";
 
 
 function LoginView(){
     return(
         <>
+        <Title />
         <Login/>
+
         </>
     )
 }
