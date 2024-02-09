b7bca0d45bb212cb2b2f01a5487a54105f537eaf
Yuliia <you@example.com>
2024-02-09 16:50:28 +0100

    Update CSS and JS code for the home page view - add animation.

--- src/View/HomePageView.jsx
+++ src/View/HomePageView.jsx
@@ -1,16 +1,31 @@
+import React, { useState, useEffect } from 'react';
+
 import HomePageBtn from '../components/ButtonForList/HomePageBtn';
 import NavBar from '../components/NavBar/NavBar';
 import Title from '../components/Title/Title'
-import './HomePageView.css'
+
+import './HomePageView.css';
+
 
 function HomePageView(){
-    return(
+
+    const [visible, setVisible] = useState(false);
+
+    const changeOpacity = () => {
+        setVisible((prevVisible) => !prevVisible);
+    };
+
+    useEffect(() => {
+        changeOpacity();
+      }, []);
+
+    return (
         <>
-        <div className='homePageHolder'>
-        <NavBar/>
-        <Title/>
-        <HomePageBtn/>
-        </div>
+            <div className={`homePageHolder ${visible ? 'visible' : ''}`}>
+                <NavBar />
+                <Title />
+                <HomePageBtn />
+            </div>
         </>
     )
 }
