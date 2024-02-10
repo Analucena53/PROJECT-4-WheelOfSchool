c5b299a87ce24642d5074f766f08af988da8da2b
lidia pérez <ufonicap@gmail.com>
2024-02-10 22:21:54 +0100

    Login View almost complete

--- src/View/LoginView.jsx
+++ src/View/LoginView.jsx
@@ -1,11 +1,12 @@
 import Login from "../components/Login/Login";
 import Title from "../components/Title/Title";
+import './loginView.css';
 
 
 function LoginView(){
     return(
         <>
-        <Title />
+        <Title/>
         <Login/>
 
         </>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,19 +1,13 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import "./login.css";
-import React from 'react';
-
-
-
 
 function Login() {
-
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
-
     alert(`Usuario: ${user}, Contraseña: ${password}`);
   }
 
@@ -25,45 +19,51 @@ function Login() {
     setPassword(event.target.value);
   }
 
-  import React from 'react';
-  import { Link } from 'react-router-dom'; // Asegúrate de importar Link desde react-router-dom
+  return (
+    <LoginForm
+      handleSubmit={handleSubmit}
+      user={user}
+      handleUserChange={handleUserChange}
+      password={password}
+      handlePasswordChange={handlePasswordChange}
+    />
+  );
+}
 
-  function LoginForm({ handleSubmit, user, handleUserChange, password, handlePasswordChange }) {
-    return (
-      <>
-        <div className="loginForm">
-          <div className="box">
-            <form onSubmit={handleSubmit}>
-              <div className="loginUser">
-                <label htmlFor="user">Usuario:</label>
-                <input
-                  type="text"
-                  id="user"
-                  name="user"
-                  value={user}
-                  onChange={handleUserChange}
-                />
-              </div>
-              <div className="PasswordUser">
-                <label htmlFor="password">Contraseña:</label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  value={password}
-                  onChange={handlePasswordChange}
-                />
-              </div>
-              <button className="buttonLogin">
-                <Link to="/home">Enviar</Link>
-              </button>
-            </form>
-          </div>
+function LoginForm({ handleSubmit, user, handleUserChange, password, handlePasswordChange }) {
+  return (
+    <>
+      <div className="loginForm">
+        <div className="box">
+          <form onSubmit={handleSubmit}>
+            <div className="loginUser">
+              <label htmlFor="user">Usuario:</label>
+              <input
+                type="text"
+                id="user"
+                name="user"
+                value={user}
+                onChange={handleUserChange}
+              />
+            </div>
+            <div className="PasswordUser">
+              <label htmlFor="password">Contraseña:</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </div>
+            <button className="buttonLogin">
+              <Link to="/home">Enviar</Link>
+            </button>
+          </form>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
+}
 
-  export default LoginForm;
-
-  export default Login;
+export default Login;
