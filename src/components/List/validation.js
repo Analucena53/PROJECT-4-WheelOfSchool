581a9a4254c4d33a479995144a05c3d8554ad203
Yuliia <you@example.com>
2024-02-12 21:15:23 +0100

    Add sweetAlert2

--- src/components/List/validation.js
+++ src/components/List/validation.js
@@ -1,7 +1,9 @@
-function Validar(user){
+import Swal from 'sweetalert2';
+import './alert.css'
 
+function Validar(user) {
     const validateEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    const resultEmail = validateEmail.test(user.userEmail); 
+    const resultEmail = validateEmail.test(user.userEmail);
 
     const validateName = /^[A-Za-z]+$/;
     const resultName = validateName.test(user.userName + user.userSurname + user.userSecondSurname);
@@ -13,20 +15,28 @@ function Validar(user){
         !user.userCourse ||
         !user.userClass ||
         !user.userEmail) {
-        alert('Rellene todos los campos obligatorios')
-        return(false);
-        
-    }
-    else if(!resultEmail) {
-        alert('Formato de correo electrónico incorrecto. Por favor, corrige el email según el siguente ejemplo: example@mail.com');
-        return (false);
-    }
-    else if(!resultName) {
-        alert('Formato de nombre incorrecto. El nombre y los apellidos deben contener sólo minúsculas y mayúsculas');
-        return (false);
-    }
-    else {
-        return(true);
+        Swal.fire({
+            imageUrl: '../public/images/iconoError.svg',
+            title: 'Oops...',
+            text: 'Rellene todos los campos obligatorios',
+        });
+        return false;
+    } else if (!resultEmail) {
+        Swal.fire({
+            imageUrl: '../public/images/iconoError.svg',
+            title: 'Oops...',
+            text: 'Formato del correo electrónico incorrecto. Por favor, corrige el email según el siguiente ejemplo: example@mail.com',
+        });
+        return false;
+    } else if (!resultName) {
+        Swal.fire({
+            imageUrl: '../public/images/iconoError.svg',
+            title: 'Oops...',
+            text: 'Formato de nombre incorrecto. El nombre y los apellidos deben contener sólo minúsculas y mayúsculas',
+        });
+        return false;
+    } else {
+        return true;
     }
 };
 
