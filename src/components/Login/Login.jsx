160d67b5bfcd2aa5f70da1663ba2fad2461593b3
lidia pérez <ufonicap@gmail.com>
2024-02-13 09:10:40 +0100

    Login Complete to merge

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,27 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Swal from 'sweetalert2';
 import "./Login.css";
 
 function Login() {
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (user === "" || password === "") {
+      Swal.fire({
+        imageUrl: "public/images/iconoError.svg",
+        imageWidth: 100,
+        imageHeight: 100,
+        title: "Oops...",
+        text: "Rellene todos los campos",
+      });
+    } else {
+    
+      window.location.href = "/Home";
+    }
+  }
 
   function handleUserChange(event) {
     setUser(event.target.value);
@@ -16,6 +33,7 @@ function Login() {
 
   return (
     <LoginForm
+      handleSubmit={handleSubmit}
       user={user}
       handleUserChange={handleUserChange}
       password={password}
@@ -24,12 +42,12 @@ function Login() {
   );
 }
 
-function LoginForm({ user, handleUserChange, password, handlePasswordChange }) {
+function LoginForm({ handleSubmit, user, handleUserChange, password, handlePasswordChange }) {
   return (
     <>
       <div className="loginForm">
         <div className="box">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="loginUser">
               <label htmlFor="user">Usuario:</label>
               <input
@@ -50,11 +68,9 @@ function LoginForm({ user, handleUserChange, password, handlePasswordChange }) {
                 onChange={handlePasswordChange}
               />
             </div>
-            <Link to="/Home">
-              <button className="buttonLogin" type="button">
+            <button className="buttonLogin" type="submit">
                 Enviar
-              </button>
-            </Link>
+            </button>
           </form>
         </div>
       </div>
